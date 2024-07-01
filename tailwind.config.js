11003802af6a98a52ac1054bb6cdf6a6236b4ea5
Andrii Skliar <andreas.skliar@gmail.com>
2024-07-01 13:07:33 +0300

    add favorites

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,ts,vue,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#F3743D',
+        gray: '#939393'
+      }
+    }
+  },
+  plugins: []
+}
